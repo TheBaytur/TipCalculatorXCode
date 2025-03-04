11826ccd69f69b927058cb9dd866401e2d4c0ceb
TheBaytur <bayturcodding@gmail.com>
2025-03-04 15:31:10 +0800

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Admin on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
